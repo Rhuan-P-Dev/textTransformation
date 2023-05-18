a12f6eca40a9fc04d3506928e1515343fb652f68
Rhuan P <noreplay@example.com>
2023-05-18 05:19:47 -0300

    feat: add CustomDoublyLinkedList

---
+++ static/js/globalClass.js
@@ -0,0 +1,73 @@
+class CustomDoublyLinkedList{
+
+    Chain = ""
+
+    firstNode = {
+        protected: true,
+        element: document.getElementById("chainInput"),
+
+        init: function(){
+            this.next.init()
+        },
+
+        next:undefined,
+        previous:null,
+    }
+
+    lastNode = {
+        protected:true,
+        lastNode:true,
+
+        element:document.getElementById("chainOutput"),
+        get: function(){this.previous.element.value},
+        init: function(){
+            this.element.innerHTML = this.get()
+        },
+
+        next:null,
+        previous:undefined,
+    }
+
+    constructor(){
+        this.firstNode.next = this.lastNode
+        this.lastNode.previous = this.firstNode
+        this.Chain = this.firstNode
+    }
+
+    add(ChainNode){
+        let node = this.Chain
+        while(1){
+            if(node.next.lastNode == true){
+                let oldNext = node.next
+
+                node.next = ChainNode
+                oldNext.previous = ChainNode
+
+                ChainNode.next = oldNext
+                ChainNode.previous = node
+
+                return
+            }else{
+                node = node.next
+            }
+        }
+    }
+    
+    remove(){
+        let node = this.Chain
+        while(1){
+            if(node.next.lastNode == true){
+
+                if(node.protected){return}
+
+                node.previous.next = node.next
+                node.next.previous = node.previous
+
+                return
+            }else{
+                node = node.next
+            }
+        }
+    }
+
+}
